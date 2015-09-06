c53608fde0af2faa120cdf65f8095dca48e56da3
Louis Pilfold <louis@lpil.uk>
2015-09-06 20:12:26 +0100

    We have a test runner

---
+++ lib/check/runner.ex
@@ -0,0 +1,49 @@
+defmodule Check.Runner do
+  @moduledoc """
+  The runner performs our tests multiple times until it encounters a failed
+  assertion, or an exception.
+  """
+
+  @num_runs 100
+  @runs 1..@num_runs
+
+  @doc """
+  Takes a test body's AST, and a keyword list of assignments, and runs the test
+  #{@num_runs} times.
+
+  The module in which the test AST was quoted must be given for the assignments
+  to be registered in the correct scope.
+  """
+  def test(test_ast, scope, assignments) do
+    ast = prepend_assignments( test_ast, scope, assignments )
+    @runs
+    |> Enum.each fn _ ->
+      values = realise_assignments( assignments )
+      Code.eval_quoted ast, values
+    end
+  end
+
+
+
+  defp realise_assignments(assignments) do
+    Enum.map assignments, fn {id, generator} ->
+      {id, generator.()}
+    end
+  end
+
+  def prepend_assignments(test_ast, scope, assignments) do
+    ast = assignment_ast( assignments, scope ) ++ [test_ast]
+    {:__block__, [], ast}
+  end
+
+  defp assignment_ast(assignments, scope) do
+    Enum.map assignments, fn {id, _} ->
+      {:=, [],
+        [
+          {:x, [], scope},
+          {:var!, [context: __MODULE__], [{id, [], __MODULE__}]},
+        ]
+      }
+    end
+  end
+end
---
+++ test/check/runner_test.exs
@@ -0,0 +1,39 @@
+defmodule Check.RunnerTest do
+  use Check.Case
+
+  alias Check.Runner
+  import ExUnit.CaptureIO
+
+  check "test evalutes given AST" do
+    ast = quote do IO.puts "Hello, world!" end
+    out = capture_io fn ->
+      Runner.test( ast, __MODULE__, [] )
+    end
+    assert String.contains?( out, "Hello, world!" )
+  end
+
+  check "test evaluates the given AST multiple times" do
+    ast = quote do IO.write "Ω" end
+    expected = String.duplicate "Ω", 100
+    out = capture_io fn ->
+      Runner.test( ast, __MODULE__, [] )
+    end
+    assert out == expected
+  end
+
+  check "assertion exceptions bubble up" do
+    ast = quote do assert false end
+    assert_raise ExUnit.AssertionError, "Expected truthy, got false", fn ->
+      Runner.test( ast, __MODULE__, [] )
+    end
+  end
+
+  check "test prepends variable assignments" do
+    ast = quote do
+      import ExUnit.Assertions
+      assert x == 1
+    end
+    assignments = [x: fn -> 1 end]
+    Runner.test( ast, __MODULE__, assignments )
+  end
+end
