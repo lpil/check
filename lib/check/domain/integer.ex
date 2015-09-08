10cb4db081823bbbb2c65ef6d18a619e520f69db
Louis Pilfold <louis@lpil.uk>
2015-09-08 22:04:47 +0100

    Correct moduledoc

--- lib/check/domain/integer.ex
+++ lib/check/domain/integer.ex
@@ -1,6 +1,6 @@
 defmodule Check.Domain.Integer do
   @moduledoc """
-  Generates a random integer between an optional max and min value.
+  The domain of integers.
   """
 
   @min -1_000_000_000_000
@@ -10,6 +10,9 @@ defmodule Check.Domain.Integer do
   def shrink(n) when n < 0 do n + 1 end
   def shrink(n)            do n     end
 
+  @doc """
+  Generates a random integer between an optional max and min value.
+  """
   def any(min \\ @min, max \\ @max)
   when is_integer(max) and is_integer(min) and max > min do
     diff = max - min
